c2f5064ee1ad0ba88e4d24f231518976c95c333b
allen <74865251a@gmail.com>
2024-03-21 20:03:47 +0800

    add firebase url image

--- test/upload_testing.tsx
+++ test/upload_testing.tsx
@@ -5,13 +5,51 @@ import { insertRow, upload } from "@/utils/firebaseHelper";
 const YourComponent = () => {
   const [file, setFile] = useState(null);
   const [loading, setLoading] = useState(false);
-  const [address, setAddress] = useState("aaa"); 
+  const [address, setAddress] = useState("bbb"); 
+  const [image1, setImage1] = useState(null);
+  const [image2, setImage2] = useState(null);
 
   const handleFileChange = (event) => {
     setFile(event.target.files[0]);
   };
 
-  const handleSaveClick = async () => {
+  const handleImage1Change = (event) => {
+    setImage1(event.target.files[0]);
+  };
+
+  const handleImage2Change = (event) => {
+    setImage2(event.target.files[0]);
+  };
+
+  const ImageSaveClick = async () => {
+    setLoading(true);
+    console.log(address)
+    try {
+      if (!address) {
+        throw new Error('No user or provider')
+      }
+      if (!image1) {
+        throw new Error('No Image 1 found')
+      }
+      if (!image2) {
+        throw new Error('No Image 2 found')
+      }
+
+      const key = `${address}-${+new Date()}`;
+      const url_1 = await upload(`${key}-image1`, image1);
+      const url_2 = await upload(`${key}-image2`, image2);
+
+      console.log(url_1);
+      console.log(url_2);
+
+      console.log('Images uploaded successfully.');
+    } catch (err) {
+      console.error(err.message);
+    }
+    setLoading(false);
+  };
+
+  const SaveVideoClick = async () => {
     setLoading(true);
     console.log(address)
     try {
@@ -19,12 +57,11 @@ const YourComponent = () => {
         throw new Error('No user or provider')
       }
       if (!file) {
-        throw new Error('No image selected')
+        throw new Error('No Video selected')
       }
 
-      
       const key = `${address}-${+new Date()}`;
-      const url = await upload(`${key}`, file)
+      const url = await upload(`${key}`, file);
 
       await insertRow('videos', [key], {
         id: key,
@@ -44,7 +81,11 @@ const YourComponent = () => {
   return (
     <div>
       <input type="file" onChange={handleFileChange} />
-      <button onClick={handleSaveClick}>上传文件</button>
+      <button onClick={SaveVideoClick}>Upload Video</button>
+      <br />
+      <input type="file" onChange={handleImage1Change} />
+      <input type="file" onChange={handleImage2Change} />
+      <button onClick={ImageSaveClick}>Upload Images</button>
       {loading && <div>Loading...</div>}
     </div>
   );
